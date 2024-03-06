3a7a021bc2ea8d73601611a42e8c0ea751cdf5b4
Synergyuniversal <synergyuniversal5@gmail.com>
2024-03-06 16:56:07 +0530

    commit

---
+++ Demo2/src/main/java/com/synergy1/Demos.java
@@ -0,0 +1,23 @@
+package com.synergy1;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Demos")
+public class Demos extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+   
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String name =request.getParameter("name");
+		PrintWriter out =response.getWriter();
+		out.println(name+"Hello Servlet!!");
+	}
+
+}
